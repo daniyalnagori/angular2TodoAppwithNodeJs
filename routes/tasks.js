6e04602bb8716bef9959e340fc54d3a8d6e080b5
daniyalnagori <daniyalnagori@yahoo.com>
2017-03-28 18:36:39 +0500

    updated

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,36 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://daniyalnagori:junaids22061996@ds033076.mlab.com:33076/todowithangular', ['tasks'])
-router.get('/tasks', function (req, res, next) {
-    db.tasks.find(function (err, tasks) {
-        if (err) {
+var db = mongojs('mongodb://daniyalnagori:junaids22061996@ds033076.mlab.com:33076/todowithangular', ['tasks']);
+
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
             res.send(err);
         }
-        res.json(tasks)
-
-    })
+        res.json(tasks);
+    });
 });
-router.get('/task/:id', function (req, res, next) {
-    db.tasks.findOne({ _id: mongojs.ObjectId(req.params.id) }, function (err, task) {
-        if (err) {
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
             res.send(err);
         }
-        res.json(task)
-    })
-})
+        res.json(task);
+    });
+});
 
-router.post('/task', function (req, res, next) {
+//Save Task
+router.post('/task', function(req, res, next){
     var task = req.body;
-    if (!task.title || (task.isDone + '')) {
+    if(!task.title || !(task.isDone + '')){
         res.status(400);
         res.json({
             "error": "Bad Data"
         });
-    }
-    else {
-        db.tasks.save(task, function (err, task) {
-            if (err) {
+    } else {
+        db.tasks.save(task, function(err, task){
+            if(err){
                 res.send(err);
             }
             res.json(task);
@@ -38,39 +41,42 @@ router.post('/task', function (req, res, next) {
     }
 });
 
-router.delete('/task/:id', function (req, res, next) {
-    db.tasks.remove({ _id: mongojs.ObjectId(req.params.id) }, function (err, task) {
-        if (err) {
+// Delete Task
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
             res.send(err);
         }
-        res.json(task)
-    })
-})
+        res.json(task);
+    });
+});
 
-router.put('/task/:id', function (req, res, next) {
+// Update Task
+router.put('/task/:id', function(req, res, next){
     var task = req.body;
     var updTask = {};
-    if (task.isDone) {
+    
+    if(task.isDone){
         updTask.isDone = task.isDone;
     }
-    if (task.title) {
+    
+    if(task.title){
         updTask.title = task.title;
     }
-    if (!updTask) {
+    
+    if(!updTask){
         res.status(400);
         res.json({
-            "error": "Bad Data"
+            "error":"Bad Data"
         });
+    } else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
     }
-    else {
-        db.tasks.update({ _id: mongojs.ObjectId(req.params.id)},updTask,{}, function (err, task) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(task)
-        })
-    }
-
-})
+});
 
 module.exports = router;
